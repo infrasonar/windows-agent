8d2e3b15087111c032de9cd2e63d97f246f90b16
Anja <anja@cesbit.com>
2023-03-09 14:52:31 +0100

    Add project files.

---
+++ WindowsAgent/Config.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAgent
+{
+    internal class Config
+    {
+        readonly String token;
+
+        public Config()
+        {
+            try
+            {
+                using (var key = Registry.LocalMachine.OpenSubKey("Software\\Wow6432Node\\InfraSonar\\Windows Agent", false))
+                {
+                    var s = key?.GetValue("Token") as string;
+                    if (!string.IsNullOrWhiteSpace(s))
+                    {
+                        this.token = s;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ WindowsAgent/ProjectInstaller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace WindowsAgent
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            ServiceProcessInstaller simpleServiceProcessInstaller = new ServiceProcessInstaller();
+            ServiceInstaller simpleServiceInstaller = new ServiceInstaller();
+
+            // Set the account properties for the service process.
+            simpleServiceProcessInstaller.Account = ServiceAccount.LocalService;
+
+            // Set the installation properties for the service.
+            // The ServiceInstaller.ServiceName must match the
+            // ServiceBase.ServiceName set in the service
+            // implementation that is installed by this installer.
+            simpleServiceInstaller.ServiceName = "InfraSonarAgent";
+            simpleServiceInstaller.DisplayName = "InfraSonar Windows Agent";
+            simpleServiceInstaller.Description = "This service is used by InfraSonar for collecting monitoring data.";
+            simpleServiceInstaller.StartType = ServiceStartMode.Automatic;
+
+            // Add the installers to the Installer collection.
+            Installers.Add(simpleServiceInstaller);
+            Installers.Add(simpleServiceProcessInstaller);
+        }
+    }
+}
