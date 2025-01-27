95e15654b5f66f3fb39b84d993e5c52a454fdab9
Anja <anja@cesbit.com>
2025-01-27 12:23:39 +0100

    Heartbeat

--- WindowsAgent/Checks/Heartbeat.cs
+++ WindowsAgent/Checks/Heartbeat.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace WindowsAgent.Checks
 {
     using Item = Dictionary<string, object>;
-    internal class Heartbeat : Check              
+    internal class Heartbeat : Check
     {
         private const int _defaultInterval = 5;  // Interval in minutes, can be overwritten with REG key.
         private const string _key = "heartbeat";  // Check key.
@@ -22,8 +22,10 @@ namespace WindowsAgent.Checks
             items[0] = new Item
             {
                 ["name"] = "heartbeat",
+                ["time"] = DateTimeOffset.UtcNow.ToUnixTimeSeconds(),
             };
             data.AddType("heartbeat", items);
             return data;
         }
+    }
 }
